f23048342c52c8099f4a8309afb2bb0d29328e0c
zj <zengjun@newgrand.cn>
2026-01-23 23:32:51 +0800

    log

--- metado-admin/src/main/java/cn/metado/admin/MetadoAdminApplication.java
+++ metado-admin/src/main/java/cn/metado/admin/MetadoAdminApplication.java
@@ -1,13 +1,27 @@
 package cn.metado.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
 @SpringBootApplication
 public class MetadoAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetadoAdminApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MetadoAdminApplication.class, args);
+
+        int definitionCount = run.getBeanDefinitionCount();
+        log.info("项目beans count：{}",definitionCount);
+
+        List<String> beanNames = Arrays.stream(run.getBeanDefinitionNames()).sorted().toList();
+        for (String definitionName : beanNames) {
+            log.info("项目bean：{}", definitionName);
+        }
     }
 
 }
