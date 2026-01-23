289bf4712c2c32eea9290f1f8365a92f51d8d501
zj <zengjun@newgrand.cn>
2026-01-23 23:05:20 +0800

    init

---
+++ metado-admin/src/main/java/cn/metado/admin/MetadoAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.metado.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetadoAdminApplication.class, args);
+    }
+
+}
---
+++ metado-admin/src/test/java/cn/metado/admin/MetadoAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.metado.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadoAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
